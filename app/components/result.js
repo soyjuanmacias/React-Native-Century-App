da4e23288ea2daa2116caa567be2c83c6af117a5
Juan Macías Opinno <juan.macias@opinno.com>
2018-05-27 10:35:00 +0200

    Result finished

--- app/components/result.js
+++ app/components/result.js
@@ -6,16 +6,17 @@ import {
   View,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import { Header } from './header';
 
 export class Result extends Component {
   _onPressBackBtn = () => {
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
     navigate('Home');
   }
   render() {
-    const {result} = this.props.navigation.state.params;
+    const { result } = this.props.navigation.state.params;
     return (
       <View style={styles.container}>
         <Image
@@ -24,51 +25,53 @@ export class Result extends Component {
         />
         <Header />
         <View style={styles.pageLayout}>
-          <View style={styles.playerInfoContainer}>
-            <View style={{ flex: 2, backgroundColor: '#FABADA', justifyContent: 'center',}}>
-              <View style={{ flexDirection: 'row', alignItems: 'center',}}>
-                <View style={{ paddingRight: 25 }}>
-                  <Image
-                    source={require('../assets/images/logo-adidas.png')}
-                    style={styles.userAvatar}
-                  />
-                </View>
-                <View>
-                  <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#0a0a39'}}>Cristiano Rolando</Text>
+          <ScrollView>
+            <View style={styles.playerInfoContainer}>
+              <View style={{ flex: 2, justifyContent: 'center', marginBottom: 5}}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                  <View style={{ paddingRight: 25 }}>
+                    <Image
+                      source={require('../assets/images/logo-adidas.png')}
+                      style={styles.userAvatar}
+                    />
+                  </View>
+                  <View>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#0a0a39' }}>Cristiano Rolando</Text>
+                  </View>
                 </View>
               </View>
+              <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row', }}>
+                <Image
+                  source={require('../assets/images/logo-adidas.png')}
+                  style={styles.iconInfo}
+                />
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#aeabd5' }}>Real Madrid</Text>
+              </View>
+              <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row', }}>
+                <Image
+                  source={require('../assets/images/logo-adidas.png')}
+                  style={styles.iconInfo}
+                />
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#aeabd5' }}>33 años</Text>
+              </View>
             </View>
-            <View style={{ flex: 1, backgroundColor: 'orange', alignItems: 'center', flexDirection: 'row',}}>
-              <Image
-                source={require('../assets/images/logo-adidas.png')}
-                style={styles.iconInfo}
-              />
-              <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#aeabd5' }}>Real Madrid</Text>
+            <View style={styles.playerResultContainer}>
+              <Text>La conclusión que hemos extraido después de pasar por nuestra herramienta de machine learning es que: </Text>
+              <Text style={{ fontSize: 21, fontWeight: 'bold', color: '#0a0a39' }}>{result.result}</Text>
             </View>
-            <View style={{ flex: 1, backgroundColor: 'black', alignItems: 'center', flexDirection: 'row',}}>
-              <Image
-                source={require('../assets/images/logo-adidas.png')}
-                style={styles.iconInfo}
-              />
-              <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#aeabd5'}}>33 años</Text>
+            <View style={styles.playerGraphicContainer}>
+              <Text >Con una probabilidad de</Text>
+              <Text style={{ fontSize: 25, fontWeight: 'bold' }}>{result.percentage}</Text>
             </View>
-          </View>
-          <View style={styles.playerResultContainer}>
-            <Text>La conclusión que hemos extraido después de pasar por nuestra herramienta de machine learning es que: </Text>
-            <Text style={{ fontSize: 21, fontWeight: 'bold', color: '#0a0a39'}}>{result.result}</Text>
-          </View>
-          <View style={styles.playerGraphicContainer}>
-            <Text >Con una probabilidad de</Text>
-            <Text style={{fontSize: 25, fontWeight: 'bold'}}>{result.percentage}</Text>
-          </View>
-          <View style={styles.btnUpload}>
-            <TouchableOpacity
-              style={styles.btnUploadInner}
-              onPress={this._onPressUploadBtn}
-            >
-              <Text style={{ color: '#7c72ff', fontSize: 22, fontWeight: 'bold', opacity: 1 }}>Enviar</Text>
-            </TouchableOpacity>
-          </View>
+            <View style={styles.btnBack}>
+              <TouchableOpacity
+                style={styles.btnBackInner}
+                onPress={this._onPressBackBtn}
+              >
+                <Text style={{ color: '#7c72ff', fontSize: 22, fontWeight: 'bold', opacity: 1 }}>Volver</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
         </View>
       </View>
     )
@@ -88,14 +91,15 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   pageLayout: {
-    flex: 5,
+    // flex: 5,
     width: '100%',
     alignItems: 'center',
     paddingHorizontal: 50,
   },
   playerInfoContainer: {
     flex: 2,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
+    opacity: 0.9,
     width: '100%',
     borderRadius: 25,
     marginBottom: 20,
@@ -104,7 +108,8 @@ const styles = StyleSheet.create({
   },
   playerResultContainer: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
+    opacity: 0.9,
     borderRadius: 25,
     marginBottom: 20,
     width: '100%',
@@ -114,7 +119,8 @@ const styles = StyleSheet.create({
   },
   playerGraphicContainer: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
+    opacity: 0.9,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
@@ -130,7 +136,7 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     marginRight: 10,
-    backgroundColor: 'red',
+    backgroundColor: 'black',
   },
   btnBack: {
     width: '80%',
@@ -138,14 +144,15 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     backgroundColor: '#7c72ff',
   },
-  btnUpload: {
+  btnBack: {
     flex: 1,
     justifyContent: 'space-around',
     flexDirection: 'column',
     width: '100%',
-    paddingHorizontal: 50
+    paddingHorizontal: 50,
+    marginBottom: 15,
   },
-  btnUploadInner: {
+  btnBackInner: {
     backgroundColor: 'white',
     opacity: 0.9,
     borderRadius: 150,
